7541ac0c7a76a3fd3e5c504d1a03fc574a1ff787
CodingWithMK <032180115@ogr.uludag.edu.tr>
2023-12-20 02:28:40 +0300

    GPU Usage Monitor added.

---
+++ Tests/gpu_usage_monitor.py
@@ -0,0 +1,32 @@
+import customtkinter
+import GPUtil
+
+
+class GPUMonitorApp(customtkinter.CTk):
+    def __init__(self):
+        super().__init__()
+
+        self.title("GPU Usage Monitor")
+
+        # GPU Usage Widgets
+        self.gpu_label = customtkinter.CTkLabel(self, text='GPU Usage:')
+        self.gpu_label.grid(row=0, column=0, padx=10, pady=5, sticky='w')
+
+        self.gpu_usage = customtkinter.CTkLabel(self, text='')
+        self.gpu_usage.grid(row=0, column=1, padx=10, pady=5, sticky='w')
+
+        self.update_gpu_info()
+
+    def update_gpu_info(self):
+        try:
+            gpu_info = GPUtil.getGPUs()[0]
+            gpu_percent = gpu_info.load * 100
+            self.gpu_usage.configure(text=f'{gpu_percent:.2f}%')
+        except Exception as e:
+            print(f'Error updating GPU info: {e}')
+
+        self.after(1000, self.update_gpu_info)
+
+if __name__ == '__main__':
+    app = GPUMonitorApp()
+    app.mainloop()
